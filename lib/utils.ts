ac8d584d573e2a02d4607ff0b70dffc21fccd877
Piyush Gupta <pkgupta.pe.ug@jadavpuruniversity.in>
2024-07-21 14:25:08 +0530

    initialised project

---
+++ lib/utils.ts
@@ -0,0 +1,7 @@
+//First Commit: Added Utilities for Aceternity UI
+import { ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
