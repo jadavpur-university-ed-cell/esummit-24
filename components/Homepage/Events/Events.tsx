f28280f262cf00de7bd17556cf1af5350a9845b0
rahul-p19 <rahulpandey.dps@gmail.com>
2024-08-29 21:14:36 +0530

    added events carousel

--- components/Homepage/Events/Events.tsx
+++ components/Homepage/Events/Events.tsx
@@ -1,11 +1,56 @@
-import React from 'react'
+"use client";
+import React, { FunctionComponent } from "react";
+import Carousel from "./Carousel";
 
-function Events() {
-  return (
-    <section id='events'>
-     <div className='h-screen  bg-black'></div>
-   </section>
-  )
+interface event {
+	name: string;
+	about: string;
 }
 
-export default Events
+const eventList: event[] = [
+	{
+		name: "One",
+		about: "Build your team",
+	},
+	{
+		name: "Two",
+		about: "Hackathon",
+	},
+	{
+		name: "Three",
+		about: "Talk show",
+	},
+	{
+		name: "Four",
+		about: "Product label design",
+	},
+	{
+		name: "Five",
+		about: "Build your team",
+	},
+	{
+		name: "Six",
+		about: "Build your team",
+	},
+	{
+		name: "Seven",
+		about: "Build your team",
+	},
+];
+
+function Events() {
+	return (
+		<>
+			<section id="events">
+				<div className="h-[120vmin] bg-[#101720] flex flex-col items-center p-[10vmin]">
+					<h1 className="text-5xl text-[#fcbf49]">Events</h1>
+					<div className="flex items-center w-full h-full grow">
+					<Carousel eventList={eventList}/>
+					</div>
+				</div>
+			</section>
+		</>
+	);
+}
+
+export default Events;
--- components/Homepage/Speakers/Title.tsx
+++ components/Homepage/Speakers/Title.tsx
@@ -1,13 +1,13 @@
 import styles from "./Title.module.css"
 const Title=()=>{
   return (<div className={styles.title}>
-    <div className={styles.titlein}>
-      <h2>
+    <div className={styles.title}>
+      <h2 className="text-4xl text-[#fcbf49] font-normal font-[Poppins]">
         Our Eminent Speakers
       </h2>
     </div>
-    <div className="button">
-      <h3>Show me All</h3>
+    <div className="speaker-button cursor-pointer">
+      <h3 className="m-auto">Show me All</h3>
     </div>
   </div>);
 }
