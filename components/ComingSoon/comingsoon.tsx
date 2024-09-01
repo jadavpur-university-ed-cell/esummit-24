35a18732f2672ce9562202fbf00912b468047fae
pi-piikachu <ahnikpurkait8846@gmail.com>
2024-09-01 14:17:11 +0530

    coming soon mobile view updated

--- components/ComingSoon/comingsoon.tsx
+++ components/ComingSoon/comingsoon.tsx
@@ -11,7 +11,7 @@ const ComingSoon = () => {
   return (
     <>
     <div className="h-screen bg-[#101720] bg-grid-white/[0.05]  md:bg-grid-large-white/[0.05] text-white">
-    <Spotlight
+      <Spotlight
         className="-top-0 left-0 sm:left-20 md:left-40 lg:left-60 lg:-top-20"
         fill="white"
       />
@@ -20,12 +20,18 @@ const ComingSoon = () => {
         fill="white"
       />
       <div className="flex flex-col items-center gap-0">
+
         <AlertText/>
-        <p className="text-center md:tracking-wider mb-0 sm:mb-4 text-sm md:text-lg lg:text-2xl">
+        <p className="text-center md:tracking-wider mb-0 sm:mb-4 text-xs sm:text-sm md:text-lg lg:text-2xl">
             JU E-Cell Presents the Fourth Edition of Our Flagship Event
         </p>
-        <Image src="/svg_pers/E-Summit_Logo_Glow.png" alt='' height={500} width={300}/>
-        <TextGenerate words="Coming Soon" className="text-center text-3xl md:text-4xl lg:text-5xl" />
+        <Image src="/svg_pers/E-Summit_Logo_Glow.png" alt='' height={500} width={300} className="hidden sm:flex"/>
+        <img
+					className="object-cover w-[250px] h-[125px] sm:w-[300px] lg:h-[165px] my-3 sm:my-0 sm:hidden"
+					src="/svg_pers/E-Summit_Logo_Glow.png"
+					alt="logo"
+				/>
+        <TextGenerate words="Coming Soon" className="text-center text-2xl sm:text-3xl md:text-4xl lg:text-5xl" />
         <a href="mailto:ecell@jadavpuruniversity.in">
             <MagicButton
               title="Sponsor Us"
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,15 +7,15 @@ import Link from "next/link";
    
    export default function Footer() {
     return (
-      <section id="footer" className="flex flex-col gap-1 justify-start w-screen items-center bg-inherit">
+      <section id="footer" className="flex flex-col  justify-start w-screen items-center bg-inherit">
         
         <img
-					className="object-cover w-[275px] h-[100px] sm:w-[350px] sm:h-[125px]"
+					className="object-cover w-[200px] h-[100px] sm:w-[350px] sm:h-[125px] mt-3 sm:mt-0"
 					src="/svg_pers/ecell.png"
 					alt="logo"
 				/>
    
-        <div className="flex sm:flex-row flex-col gap-0 sm:gap-0 sm:justify-between rounded-lg shadow mb-4 bg-gray-800 min-w-[95vw] p-1 max-w-[90vw] sm:max-w-[100vw]">
+        <div className="flex sm:flex-row flex-col gap-1 sm:gap-0 sm:justify-between rounded-lg shadow mb-4 bg-gray-800 min-w-[95vw] p-1 max-w-[90vw] sm:max-w-[100vw]">
 
           <div className="w-full mx-auto max-w-screen-xl  flex items-center justify-center sm:justify-between">
             <span className="sm:text-sm text-xs text-center text-gray-400">© 2024
@@ -27,20 +27,20 @@ import Link from "next/link";
           <div className="text-gray-400 grid grid-cols-4 gap-3 mx-auto sm:mr-3">
 
             <Link href="https://www.linkedin.com/school/juecell" target="blank">
-              <FaLinkedin className="footer-icons text-lg transition-colors duration-150 ease-in-out hover:text-black"/>
+              <FaLinkedin className="footer-icons text-xs sm:text-lg transition-colors duration-150 ease-in-out hover:text-black"/>
             </Link>
 
 
             <Link href="https://www.facebook.com/juecell" target="blank">
-              <FaFacebook className="footer-icons text-lg transition-colors duration-150 ease-in-out hover:text-black" />
+              <FaFacebook className="footer-icons text-xs sm:text-lg transition-colors duration-150 ease-in-out hover:text-black" />
             </Link>
 
             <Link href="https://www.instagram.com/ju/" target="blank">
-              <FaInstagram className="footer-icons text-lg absolute transition-colors duration-150 ease-in-out hover:text-black"/>
+              <FaInstagram className="footer-icons text-xs sm:text-lg absolute transition-colors duration-150 ease-in-out hover:text-black"/>
             </Link>
 
             <Link href="https://twitter.com/ju_ecell" target="blank">
-              <BsTwitterX className="footer-icons text-lg  transition-colors duration-150 ease-in-out hover:text-black" />
+              <BsTwitterX className="footer-icons text-xs sm:text-lg  transition-colors duration-150 ease-in-out hover:text-black" />
             </Link>
           </div>
         </div>
