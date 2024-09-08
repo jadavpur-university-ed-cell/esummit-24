1cd73d6609e0dd4b92aac68d1c38550d7d7a6f53
rahul-p19 <rahulpandey.dps@gmail.com>
2024-09-08 14:52:43 +0530

    added add and edit team options

---
+++ app/admin/users/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function page() {
+  return (
+    <>
+    <h1 className='text-white text-3xl'>Show list of users here</h1>
+    </>
+  )
+}
+
+export default page
---
+++ app/api/users/checkMember/route.ts
@@ -0,0 +1,63 @@
+import { PrismaClient } from "@prisma/client";
+import { NextResponse, NextRequest } from "next/server";
+
+const prisma = new PrismaClient();
+
+export async function POST(req: NextRequest, res: NextResponse) {
+	const body = await req.json();
+	const { email, eventName, newTeam, teamId } = body;
+	// taking newTeam bool because we need different checks for editTeam and createTeam functions
+	try {
+		const user = await prisma.user.findUnique({
+			where: { email },
+		});
+
+		if (!user) {
+			return NextResponse.json({ error: "User not found" }, { status: 404 });
+		}
+
+		if(!user.emailVerified){ // checking if email is verified
+			return NextResponse.json({error:"Email not verified."},{status:400});
+		}
+
+		if(!user.isVerified){ // checking if college is verified
+			return NextResponse.json({error:"User's college not verified"},{status:400})
+		}
+
+		// checking if user already has a team
+		const existingTeam = await prisma.team.findFirst({
+			where: {
+				eventName,
+				OR: [
+					{ member1Id: user.id }, // eventName should be same, and user can be member1 or member2,3,4, so checking all ids
+					{ member2Id: user.id },
+					{ member3Id: user.id },
+					{ member4Id: user.id },
+				],
+			},
+		});
+		if (newTeam) {
+			// if it is a new team, the user must not be in any team for that event
+			if (existingTeam) {
+				return NextResponse.json(
+					{ error: "User is already part of a team for this event" },
+					{ status: 400 }
+				);
+			}
+		}
+		// if it isn't a new team, i.e. editTeam has been called, user can be a part of that team but no other team.
+		else if (existingTeam && existingTeam.teamId !== teamId) {
+			return NextResponse.json(
+				{ error: "User is already part of a team for this event" },
+				{ status: 400 }
+			);
+		}
+
+		return NextResponse.json({ user }, { status: 200 });
+	} catch (error) {
+		return NextResponse.json(
+			{ error: "Error checking user", details: (error as Error).message },
+			{ status: 404 }
+		);
+	}
+}
