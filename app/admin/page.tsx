1b505bf62ddfce6dcd70f0d5f4b600ca6d87af68
rahul-p19 <rahulpandey.dps@gmail.com>
2024-09-12 18:18:43 +0530

    minor fix

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -6,7 +6,7 @@ function page() {
     <>
     <section className='h-screen flex flex-col p-10'>
       <div className='w-full flex justify-center'>
-    <h1 className='text-white text-4xl'>Admin Page</h1>
+    <h1 className='text-white text-4xl font-semibold'>Admin Page</h1>
       </div>
       <div className='flex w-full justify-around pt-10'>
         <Link href="/admin/teams" className='bg-white px-2 py-1 rounded-[3px]'>Teams</Link>
