32b97fc870c32b75d779272e721546d54018de54
tester <test@gmail.com>
2024-09-21 18:20:09 +0530

    testpush

--- app/events/corporateclash/page.tsx
+++ app/events/corporateclash/page.tsx
@@ -1,11 +1,12 @@
 import CorporateClashEvent from "./clientCode"
+import {auth} from "@/auth"
 export default async function (){
   //most of the await calls;
-  const res:boolean = await new Promise((r)=>{
-    setTimeout(()=>{
-      r(false);
-    },500);
+  const res:boolean = await new Promise(async (r)=>{
+    const session = await auth();
+    if(!session){r(false);return;}
   })
+  console.log(res);
   return(
     <CorporateClashEvent allow={res}></CorporateClashEvent>
   )
